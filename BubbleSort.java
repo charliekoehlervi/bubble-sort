1ac0fc6ce2c68cbc024164ed2b0799be21e229fe
Charlie Koehler VI <44848908+charliekoehlervi@users.noreply.github.com>
2019-02-19 09:53:20 -0500

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class BubbleSort
+{
+    public static void main(String[] args)
+    {
+        // variables and constants
+        int[] someNums = new int[5];
+        int comparisonsToMake = someNums.length - 1;
+        Scanner input = new Scanner(System.in);
+        int a, b, temp;
+
+        for (a = 0; a < someNums.length; a++)
+        {
+            System.out.print("Enter number " + (a + 1) + " >> ");
+            someNums[a] = input.nextInt();
+        }
+
+        display(someNums, 0);
+        // the Bubble Sort Algorithm
+        for (a = 0; a < someNums.length - 1; a++)
+        {
+            for (b = 0; b < comparisonsToMake; b++)
+            {
+                if (someNums[b] > someNums [b + 1])
+                {
+                    temp = someNums[b];
+                    someNums[b] = someNums[b + 1];
+                    someNums[b + 1] = temp;
+                }
+            }
+            display(someNums, (a + 1));
+            comparisonsToMake--;
+        }
+    }
+
+    public static void display(int[] someNums, int a)
+    {
+        System.out.print("Iteration " + (a + 1) + ": ");
+        for (int x = 0; x < someNums.length; x++)
+        {
+            System.out.print(someNums[x] + " ");
+        }
+        System.out.println();
+    }
+}
